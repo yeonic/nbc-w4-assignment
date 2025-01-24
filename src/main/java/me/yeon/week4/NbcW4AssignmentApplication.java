e768b44df3ed7a9f45929e3d690ce23b78589265
김종연 <bellopenerr@gmail.com>
2025-01-24 11:57:05 +0900

    initial commit

---
+++ src/main/java/me/yeon/week4/NbcW4AssignmentApplication.java
@@ -0,0 +1,13 @@
+package me.yeon.week4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcW4AssignmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NbcW4AssignmentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/yeon/week4/NbcW4AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yeon.week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcW4AssignmentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

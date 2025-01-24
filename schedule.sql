86b0b1108fb21cb662a77c7157d034996bb48d80
김종연 <bellopenerr@gmail.com>
2025-01-24 14:54:53 +0900

    chore: add defult timestamp

--- schedule.sql
+++ schedule.sql
@@ -3,8 +3,8 @@ create table author
     author_id  bigint       not null auto_increment,
     name       varchar(15)  not null,
     email      varchar(255) not null,
-    created_at timestamp    not null,
-    updated_at timestamp    not null,
+    created_at timestamp    not null default current_timestamp,
+    updated_at timestamp    not null default current_timestamp on update current_timestamp,
     primary key (author_id)
 );
 
@@ -14,8 +14,8 @@ create table schedule
     author_id   bigint(20)   not null,
     password    int(11)      not null,
     todo        varchar(255) not null,
-    created_at  timestamp    not null,
-    updated_at  timestamp    not null,
+    created_at  timestamp    not null default current_timestamp,
+    updated_at  timestamp    not null default current_timestamp on update current_timestamp,
     primary key (schedule_id),
     constraint foreign key (author_id)
         references author (author_id)

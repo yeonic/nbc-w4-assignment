a9982bdea7289591fe99e893d1084469957d8f05
김종연 <bellopenerr@gmail.com>
2025-02-03 12:08:55 +0900

    chore: sql script 업데이트

--- schedule.sql
+++ schedule.sql
@@ -1,22 +1,26 @@
 create table user
 (
-    user_id    bigint       not null auto_increment,
-    name       varchar(15)  not null,
-    email      varchar(255) not null,
-    created_at timestamp    not null default current_timestamp,
-    updated_at timestamp    not null default current_timestamp on update current_timestamp,
-    primary key (user_id)
+    user_id    bigint auto_increment
+        primary key,
+    name       varchar(15)                         not null,
+    email      varchar(255)                        not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    updated_at timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP
 );
 
 create table schedule
 (
-    schedule_id bigint(20)   not null auto_increment,
-    user_id     bigint(20)   not null,
-    password    varchar(8)   not null,
-    todo        varchar(255) not null,
-    created_at  timestamp    not null default current_timestamp,
-    updated_at  timestamp    not null default current_timestamp on update current_timestamp,
-    primary key (schedule_id),
-    constraint foreign key (user_id)
-        references user (user_id)
+    schedule_id bigint auto_increment
+        primary key,
+    user_id     bigint                              not null,
+    password    varchar(80)                         null,
+    todo        varchar(255)                        not null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null,
+    updated_at  timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    constraint schedule_ibfk_1
+        foreign key (user_id) references user (user_id)
 );
+
+create index user_id
+    on schedule (user_id);
+
